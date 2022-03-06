017095178c33412f8b90f4cf74fc012a7e5b2da5
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-03-06 15:17:59 +0600

    3March

--- class1.cpp
+++ class1.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
+struct person
+{
+    string name_s;
+    int id_s;
+    int salary_s;
+};
+bool mycomp_dsc(person a, person b)
+{
+    if (a.salary_s == b.salary_s)
+        return a.id_s > b.id_s;
+
+    else
+        return a.salary_s > b.salary_s;
+}
+bool mycomp_asc(person a, person b)
+{
+    if (a.salary_s == b.salary_s)
+        return a.id_s < b.id_s;
+
+    else
+        return a.salary_s < b.salary_s;
+}
 int main()
 {
     int n;
     cout << "Enter number of inputs" << endl;
     cin >> n;
+    struct person people[n];
 
     string name[n];
     int id[n];
     int slary[n];
-    cout << "Enter names "<<endl;
+    cout << "Enter names " << endl;
+    cin.ignore();
     for (int i = 0; i < n; i++)
     {
-        cin.ignore();
+
         getline(cin, name[i]);
+        people[i].name_s = name[i];
     }
     cout << "Enter id  " << endl;
 
@@ -23,6 +48,7 @@ int main()
     {
 
         cin >> id[i];
+        people[i].id_s = id[i];
     }
     cout << "Enter salary" << endl;
 
@@ -30,6 +56,32 @@ int main()
     {
 
         cin >> slary[i];
+        people[i].salary_s = slary[i];
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << "Name " << name[i] << "\tId " << id[i] << "\tSalary" << slary[i] << endl;
+    // }
+    cout << "Printing using structure descending order" << endl;
+
+    sort(people, people + n, mycomp_dsc);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << people[i].name_s << "\t";
+        cout << people[i].id_s << "\t";
+        cout << people[i].salary_s << "\t";
+        cout << " " << endl;
+    }
+      cout << "Printing using structure ascending order" << endl;
+     sort(people, people + n, mycomp_asc);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << people[i].name_s << "\t";
+        cout << people[i].id_s << "\t";
+        cout << people[i].salary_s << "\t";
+        cout << " " << endl;
     }
 
     return 0;
