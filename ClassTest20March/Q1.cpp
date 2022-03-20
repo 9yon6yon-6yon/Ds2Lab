8fbbecd0e4cb9769f6861c2888c16514c714819b
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-03-20 16:21:49 +0600

    CT solve

--- ClassTest20March/Q1.cpp
+++ ClassTest20March/Q1.cpp
@@ -2,7 +2,14 @@
 // 011201366
 // Md Asif Karim Chayon
 using namespace std;
-
+void reverse(string s, int n, int i)
+{
+    if (n == i)
+        return;
+    reverse(s, n, i + 1);
+    if (n > i)
+        cout << s[i] << s[i - 1] << " ";
+}
 int main()
 {
     char str[20];
@@ -12,14 +19,16 @@ int main()
     {
         int m;
         cin >> m;
-        char newString[m];
-        for (int j = 0; j < m; j++)
-            cin >> newString[j];
+        // char newString[m];
+        //  for (int j = 0; j < m; j++)
+        string newString;
+        cin >> newString;
+        reverse(newString, m, 1);
 
-        for (int j = m - 1; j >= 1; j--)
-        {
-            cout << newString[j] << newString[j - 1] << " ";
-        }
+        // for (int j = m - 1; j >= 1; j--)
+        // {
+        //     cout << newString[j] << newString[j - 1] << " ";
+        // }
 
         cout << "\n";
     }
