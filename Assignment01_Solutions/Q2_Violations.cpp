d29cb26d3a2367626a8ebdde8eef010cc48df76f
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-03-20 14:12:22 +0600

    Assignment

--- Assignment01_Solutions/Q2_Violations.cpp
+++ Assignment01_Solutions/Q2_Violations.cpp
@@ -2,58 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[], int left[], int right[])
-{
-    int i = 0, j = 0;
-    long count = 0;
-    int l = sizeof(left) / sizeof(left[0]);
-    int r = sizeof(right) / sizeof(right[0]);
-
-    while (i < l || j < r)
-    {
-        // Loop till all elements in both subarrays are taken
-        if (i == l)
-        {
-            arr[i + j] = right[j];
-            j++; // Left subarray was fully traversed
-        }
-        else if (j == r)
-        {
-            arr[i + j] = left[i];
-            i++; // Right subarray was fully traversed
-        }
-        else if (left[i] <= right[j])
-        {
-            arr[i + j] = left[i];
-            i++; // Left element is less than Right element
-            // Hence, no inversion. No count increment.
-        }
-        else
-        {
-            arr[i + j] = right[j];
-            count += l - i;
-            // Increment count with the number of inversions
-            j++;
-        }
-    }
-    cout << count << endl;
-}
-void MERGESORT(int arr[], int lb, int ub)
-{
-    if (lb < ub)
-    {
-        int mid = (lb + ub) / 2;
-        MERGESORT(arr, lb, mid);
-        MERGESORT(arr, mid + 1, ub);
-      //  merge(arr, lb, ub);
-    }
-}
-
 int main()
 {
     int arr[10];
     int len = sizeof(arr) / sizeof(arr[0]);
-    MERGESORT(arr, 0, len - 1);
+  
 
     return 0;
 }
---
+++ Assignment01_Solutions/Q4_Rev.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n, i, j, count = 0;
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > arr[i] * 2)
+                count++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
