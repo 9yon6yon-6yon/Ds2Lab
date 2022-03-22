3c4af88b042fe1bbe6ac7e639779bb63b7a11c51
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-03-22 20:11:29 +0600

    Violations Solution using insertion sort

--- Assignment01_Solutions/Q2_Violations.cpp
+++ Assignment01_Solutions/Q2_Violations.cpp
@@ -1,12 +1,36 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+int violations(int data[], int n)
+{ // using insertion sort
+    int i, key, j;
+    int count = 0;
+    for (i = 1; i < n; i++)
+    {
+        key = data[i];
+        j = i - 1;
+
+        while (j >= 0 && data[j] > key)
+        {
+            data[j + 1] = data[j];
+            j = j - 1;
+            count++;
+        }
+        data[j + 1] = key;
+    }
+    return count;
+}
 
 int main()
 {
-    int arr[10];
-    int len = sizeof(arr) / sizeof(arr[0]);
-  
-
+    int n;
+    cin >> n;
+    int arr[n];
+    // int len = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int res = violations(arr, n);
+    cout << "\n"
+         << res << endl;
     return 0;
 }
