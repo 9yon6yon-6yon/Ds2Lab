4ab07cfed16fca0a5f483e3d1c2bc8b735ac89f4
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-05-11 21:31:44 +0600

    Assignment02

---
+++ Assignment02/Q3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int T;
+    while (T > 0)
+    {
+        int N, M;
+        cin >> T;
+        cin >> N >> M;
+
+        for (int i = 0; i < M; i++)
+        {
+        }
+        int Q;
+        cin >> Q;
+        int A, K;
+        for (int i = 0; i < Q; i++)
+        {
+            cin >> A >> K;
+        }
+        T--;
+    }
+
+    return 0;
+}
