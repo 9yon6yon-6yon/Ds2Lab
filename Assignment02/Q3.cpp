c7e067227b85012528cbb8dc1ed61f99994bf5e9
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-05-11 21:38:39 +0600

    Q3 added edges

--- Assignment02/Q3.cpp
+++ Assignment02/Q3.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef pair<int, int> iPair;
+
+struct Graph
+{
+    int X, Y;
+    vector<pair<int, iPair>> edges;
+
+    Graph(int V, int E)
+    {
+        this->X = V;
+        this->Y = E;
+    }
+
+    void addEdge(int u, int v, int w)
+    {
+        edges.push_back({w, {u, v}});
+    }
+};
+
 int main()
 {
 
@@ -10,9 +29,13 @@ int main()
         int N, M;
         cin >> T;
         cin >> N >> M;
+        Graph g(N, M);
 
         for (int i = 0; i < M; i++)
         {
+            int X, Y, C;
+            cin >> X >> Y >> C;
+            g.addEdge(X, Y, C);
         }
         int Q;
         cin >> Q;
